c92fe801ce9d7fb9227afab1c3126d55b7d0294a
EEGdecoding <109576028+EEGdecoding@users.noreply.github.com>
2022-07-25 01:48:32 +0800

    Add files via upload

--- SBLEST.m
+++ SBLEST.m
@@ -1,43 +1,43 @@
-function [W, alpha, V, Cov_mean_train] = SBLEST(X, Y, Maxiters, K, tau, e)
-% ************************************************************************
-% SBLEST    : Spatio-Temporal-filtering-based single-trial EEG classification
+function [W, alpha, V, Wh] = SBLEST(X, Y, K, tau)
+% SBLEST    : Sparse Bayesina Learning for End-to-end Spatio-Temporal-filtering-based single-trial EEG classification
+%
+% Syntax:
+%  [W, alpha, V, Wh] = SBLEST(X, Y, K, tau)
 %
 % --- Inputs ---
-% Y         : Observed label vector
-% X         : M EEG signals from train set
-%             M cells: Each X{1,i} represents a trial with size of [C*T]
-% Maxiters  : Maximum number of iterations (5000 is suggested in this paper)
-% K         : Order of FIR filter
-% tau       : time delay parameter
-% e         : Convergence threshold 
+% Y         : True label vector. [M, 1].
+% X         : M trials of C (channel) x T (time) EEG signals. [C, T, M].
+% K         : Order of FIR filter.
+% tau       : Time delay parameter.
 %
-% --- Output ---
-% W         : The estimated low-rank matrix
-% alpha     : The classifier parameter (eigenvalues of W)
-% V         : Each column of V represents a spatio-temporal filter (eigenvectors of W)
-% Cov_mean_train : Averaged covariance matrix of train set (2 C^2 * 2 C^2)
-%                  (using for whittening and logarithm transform in test stage)
+% --- Outputs ---
+% W         : Estimated low-rank weight matrix. [K*C, K*C].
+% alpha     : Classifier weights. [L, 1].
+% V         : Spatio-temporal filter matrix. [K*C, L].
+%             Each column of V represents a spatio-temporal filter.
+% Wh        : Whitening matrix for enhancing covariance matrices (required for prediction on test set). [(K*C)^2, (K*C)^2].
 
 % Reference:
-
-% Copyright:
+%¡°W. Wang, F. Qi, D. Wipf, C. Can, T. Yu, Z. Gu, E. N. Brown, Z. Yu, W. Wu. Sparse Bayesian Learning for End-to-End EEG Decoding (under review)¡±
+%
+% Wenlong Wang, Feifei Qi, Wei Wu, 2022.
+% Email: 201710102248@mail.scut.edu.cn
 
 % ************************************************************************
-
-[R_train, Cov_mean_train] = Enhanced_cov_train(X, K, tau);
+% Compute enhanced covariace matrices and whitening matrix
+[R_train, Wh] = Enhanced_cov_train(X, K, tau);
 
 %% Check properties of R
 [M, D_R] = size(R_train); % M: # of samples; D_R: dimention of vec(R_m)
 KC = round(sqrt(D_R));
-epsilon = e;
 Loss_old = 1e12;
-threshold = 0.05; % 
+threshold = 0.05; %
 if (D_R ~= KC^2)
     disp('ERROR: Columns of A do not align with square matrix');
     return;
 end
 
-% Check properties of R: symmetric ?
+% Check if R is symmetric
 for c = 1:M
     row_cov = reshape(R_train(c,:), KC, KC);
     if ( norm(row_cov - row_cov','fro') > 1e-4 )
@@ -47,101 +47,101 @@ for c = 1:M
 end
 
 %% Initializations
-U = zeros(KC,KC); % The estimated low-rank matrix W set to be Zeros
-Psi = eye(KC); % the covariance matrix of Gaussian prior distribution is initialized to be Unit diagonal matrix
-lambda = 1;% the variance of the additive noise set to 1 by default
- 
+U = zeros(KC, KC); % estimated low-rank matrix W initialized to be Zeros
+Psi = eye(KC); % covariance matrix of Gaussian prior distribution is initialized to be unit diagonal matrix
+lambda = 1;% variance of the additive noise set to 1
+
 %% Optimization loop
-for i = 1:Maxiters
-   %% Compute estimate of X 
-    RPR = zeros(M, M); %  Predefined temporal variables RT*PSI*R
-    B = zeros(KC^2, M); %  Predefined temporal variables
+for i = 1:5000
+    %% Update U
+    RPR = zeros(M, M);
+    B = zeros(KC^2, M);
     for c = 1:KC
         start = (c-1)*KC + 1; stop = start + KC - 1;
-        Temp = Psi*R_train(:,start:stop)'; 
-        B(start:stop,:)= Temp; 
-        RPR =  RPR + R_train(:,start:stop)*Temp;  
+        Temp = Psi*R_train(:, start:stop)';
+        B(start:stop,:) = Temp;
+        RPR =  RPR + R_train(:, start:stop)*Temp;
     end
-
-    Sigma_y = RPR + lambda*eye(M); 
-    u = B*( Sigma_y\Y ); % Maximum a posterior estimation of u
+    
+    Sigma_y = RPR + lambda*eye(M);
+    u = B*(Sigma_y\Y ); % maximum a posterior estimation of u
     U = reshape(u, KC, KC);
     U = (U + U')/2; % make sure U is symmetric
-       
-   %% Update the dual variables of PSI : PHi_i
-    Phi = cell(1,KC);
+    
+    %% Update Phi (dual variable of Psi)
+    Phi = cell(1, KC);
     SR = Sigma_y\R_train;
     for c = 1:KC
         start = (c-1)*KC + 1; stop = start + KC - 1;
         Phi{1,c} = Psi - Psi * ( R_train(:,start:stop)' * SR(:,start:stop) ) * Psi;
     end
     
-    %% Update covariance parameters Psi: Gx
-    PHI = 0;    
+    %% Update Psi
+    PHI = 0;
     UU = 0;
     for c = 1:KC
-        PHI = PHI +  Phi{1,c};
+        PHI = PHI +  Phi{1, c};
         UU = UU + U(:,c) * U(:,c)';
     end
     Psi = ( (UU + UU')/2 + (PHI + PHI')/2 )/KC; % make sure Psi is symmetric
-
-   %% Update lambda
-   theta = 0;
+    
+    %% Update theta (dual variable of lambda) and lambda
+    theta = 0;
     for c = 1:KC
         start = (c-1)*KC + 1; stop = start + KC - 1;
         theta = theta +trace(Phi{1,c}* R_train(:,start:stop)'*R_train(:,start:stop)) ;
     end
-    lambda = (sum((Y-R_train*u).^2) + theta)/M;  
-
-   %% Output display and  convergence judgement
-        Loss = Y'*Sigma_y^(-1)*Y + log(det(Sigma_y));         
-        delta_loss = abs(Loss_old-Loss)/abs( Loss_old);
-        if (delta_loss < epsilon)
-            disp('EXIT: Change in Loss below threshold');
-            break;
-        end
-        Loss_old = Loss;
-         if (~rem(i,100))
-            disp(['Iterations: ', num2str(i),  '  lambda: ', num2str(lambda),'  Loss: ', num2str(Loss), '  Delta_Loss: ', num2str(delta_loss)]);
-         end   
+    lambda = (sum((Y-R_train*u).^2) + theta)/M;
+    
+    %% Convergence check
+    Loss = Y'*Sigma_y^(-1)*Y + log(det(Sigma_y));
+    delta_loss = abs(Loss_old-Loss)/abs( Loss_old);
+    if (delta_loss < 2e-4)
+        disp('EXIT: Change in loss below threshold');
+        break;
+    end
+    Loss_old = Loss;
+    if (~rem(i,100))
+        disp(['Iterations: ', num2str(i),  '  lambda: ', num2str(lambda),'  Loss: ', num2str(Loss), '  Delta_Loss: ', num2str(delta_loss)]);
+    end
 end
-    %% Eigendecomposition of W
-     W = U;
-     [~, D, V_all] = eig(W); % each column of V represents a spatio-temporal filter
-     alpha_all = diag(D); % classifier parameters
-     %% Select L pairs of spatio-temporal filters V and classifier parameters alpha
-    d = abs(diag(D)); d_max = max(d); 
-    w_norm = d/d_max; % normalize eigenvalues of W by the maximum eigenvalue
-    index = find(w_norm > threshold); % find index of selected V by pre-defined threshold,.e.g., 0.05
-    V = V_all(index); alpha = alpha_all(index);% select V and alpha by index   
+%% Eigendecomposition of W
+W = U;
+[~, D, V_all] = eig(W); % each column of V represents a spatio-temporal filter
+alpha_all = diag(D); % classifier weights
+%% Determine spatio-temporal filters V and classifier weights alpha
+d = abs(diag(D)); d_max = max(d);
+w_norm = d/d_max; % normalize eigenvalues of W by the maximum eigenvalue
+index = find(w_norm > threshold); % indices of selected V according to a pre-defined threshold,.e.g., 0.05
+V = V_all(index); alpha = alpha_all(index);
 end
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [R_train, Cov_mean_train] = Enhanced_cov_train(X, K, tau)
-% ************************************************************************
-% Compute Enhanced covariace matrix of train set
-% Input :
+function [R_train, Wh] = Enhanced_cov_train(X, K, tau)
+% Compute enhanced covariace matrices
+%
+% Inputs :
+% X         : M trials of C (channel) x T (time) EEG signals. [C, T, M].
 % K         : Order of FIR filter
-% tau       : time delay parameter
-% X         : EEG signals from train set, # trials is M
+% tau       : Time delay parameter
 
-% Output    :
-% R         :Enhanced covariace matrix of train set (M * 4 C^2)
-% Cov_mean_train : Averaged covariance matrix of train set (2 C^2 * 2 C^2)
-%                 (using for whittening and logarithm transform in test stage)
+
+% Outputs    :
+% R         : Enhanced covariace matrices. [M,(K*C)^2*(K*C)^2 ]
+% Wh        : Whitening matrix. [(K*C)^2, (K*C)^2].
+
 % ************************************************************************
 
-%  Initializaiton 
-M = length(X); % M: # trials
-[C,T] = size(X{1,1}); % C: # recorded channels, T: # sampled time points
-KC = K*C; % KC: # augmented covariance matrices
+%  Initializaiton
+[C, T, M] = size(X); 
+KC = K*C; % [KC, KC]: dimension of augmented covariance matrix
 Cov = cell(1, M);
 Sig_Cov = zeros(KC, KC);
 for m = 1:M
-    X_m = X{1,m};
+    X_m = X(:,:,m);
     X_m_hat = [];
-     
-    % Generate augument EEG data
+    
+    % Generate augumented EEG data
     for k = 1 : K
         n_delay = (k-1)*tau;
         if n_delay == 0
@@ -155,21 +155,21 @@ for m = 1:M
     
     % Compute covariance matrices with trace normalizaiton
     Cov{1,m} = X_m_hat*X_m_hat';
-    Cov{1,m}= Cov{1,m}/trace(Cov{1,m}); 
+    Cov{1,m} = Cov{1,m}/trace(Cov{1,m});
     Sig_Cov = Sig_Cov + Cov{1,m};
 end
 
-% compute averaged covariance matrix of training set
-Cov_mean_train = Sig_Cov/M;
+% Compute Whitening matrix
+Wh = Sig_Cov/M;
 
-% Whitenning, Logarithm transform, and Vectorization
+% Whitening, logarithm transform, and Vectorization
 Cov_whiten = zeros(M, KC, KC);
 for m = 1:M
-    temp_cov = Cov_mean_train^(-1/2)*Cov{1,m}*Cov_mean_train^(-1/2);% whiten
-    Cov_whiten(m,:,:)  = (temp_cov + temp_cov')/2; 
-    R_m =logm(squeeze(Cov_whiten(m,:,:))); % logarithm transform
+    temp_cov = Wh^(-1/2)*Cov{1,m}*Wh^(-1/2);% whitening
+    Cov_whiten(m,:,:)  = (temp_cov + temp_cov')/2;
+    R_m = logm(squeeze(Cov_whiten(m,:,:))); % logarithm transform
     R_m = R_m(:); % column-wise vectorization
     R_train(m,:) =  R_m';
-end 
+end
 end
 
--- SBLEST_main.m
+++ SBLEST_main.m
@@ -1,55 +1,51 @@
-
+%%% An example code for classifying single-trial EEG data using SBLEST
 clc; clear; close all;
 
-%% load data : subject L1 from Dataset II ( "foot" vs "tongue")
+%% Load data: subject L1 from Dataset II ( "foot" vs "tongue")
 load('Dataset2_L1_FootTongue_train.mat');
 load('Dataset2_L1_FootTongue_test.mat');
-tau_selected = 1; % "tau_selected =1" for this subset is selected from CV
+tau_selected = 1; % this was determined based on 10-fold cross-validation on the training set
 
 %% Initialization
-Maxiters = 5000; % Pre-defined maximum iterations (5000 in this paper)
-e = 2e-4; % Pre-defined threshold for break condition (2e-4 in this papter)
 tau = tau_selected;
 if tau == 0
     K = 1;
 else
     K = 2;
 end
-%% Trainning stage: run SBLEST 
-disp(['Order: ', num2str(K),  '      Time delay: ', num2str(tau)]);
-disp('Running SBLEST : update W, psi and lambda');
-[W, alpha, V, Cov_mean_train] = SBLEST(X_train, Y_train, Maxiters, K, tau, e);
+%% Training stage: run SBLEST on the training set
+disp(['FIR filter order: ', num2str(K),  '      Time delay: ', num2str(tau)]);
+disp('Running SBLEST : update W, Psi and lambda');
+[W, alpha, V, Wh] = SBLEST(X_train, Y_train, K, tau);
 
-%% Test stage : predicte test label
-[R_test] = Enhanced_cov_test(X_test, K, tau, Cov_mean_train);
+%% Test stage : predicte labels in the test set
+R_test = Enhanced_cov_test(X_test, K, tau, Wh);
 predict_Y = R_test*vec(W);
-[accuracy] = compute_acc (predict_Y, Y_test);
+accuracy = compute_acc (predict_Y, Y_test);
 disp(['Test   Accuracy: ', num2str(accuracy)]);
 
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [R_test] = Enhanced_cov_test(X,K,tau,Cov_mean_train)
-% ************************************************************************
-% Compute Enhanced covariace matrix of test set
-% Input :
+function R_test = Enhanced_cov_test(X, K, tau, Wh)
+% Compute enhanced covariace matrices of test set
+%
+% Inputs :
+% X         : EEG signals ofm test set
 % K         : Order of FIR filter
-% tau       : time delay parameter
-% X         : M EEG signals from test set
+% tau       : Time delay parameter
+% Wh        : Whitening matrix for enhancing covariance matrices
 
 % Output    :
-% R_test    : Enhanced covariace matrix of test set
+% R_test    : Enhanced covariace matrices of test set
 % ************************************************************************
-%  Initializaiton 
-M = length(X); 
-[C,T] = size(X{1,1});
-KC = K*C;
-Cov = cell(1,M);
-Sig_Cov = zeros(KC,KC);
-
+[C, T, M] = size(X);
+KC = K*C; % [KC, KC]: dimension of augmented covariance matrix
+Cov = cell(1, M);
+Sig_Cov = zeros(KC, KC);
 for m = 1:M
-    X_m = X{1,m};
-    X_m_hat = [];  
-    % Generate augument EEG data
+    X_m = X(:,:,m);
+    X_m_hat = [];
+    % Generate augumented EEG data
     for k = 1:K
         n_delay = (k-1)*tau;
         if n_delay ==0
@@ -60,34 +56,26 @@ for m = 1:M
         end
         X_m_hat = cat(1,X_m_hat,X_order_k);
     end
-     % Compute covariance and trace normalizaiton
+    % Compute covariance and trace normalization
     Cov{1,m} = X_m_hat*X_m_hat';
-    Cov{1,m}= Cov{1,m}/trace(Cov{1,m}); 
+    Cov{1,m} = Cov{1,m}/trace(Cov{1,m});
     Sig_Cov = Sig_Cov + Cov{1,m};
 end
 
-% Whitenning, logarithm transform and reshape
-Cov_whiten = zeros(M,KC,KC);
-for m = 1:M
-    Cov_whiten(m,:,:) = Cov_mean_train^(-1/2)*Cov{1,m}*Cov_mean_train^(-1/2);
-    R_test(m,:) =  vec(logm(squeeze(Cov_whiten(m,:,:))));
-end 
-
-
-% Whitenning, Logarithm transform, and Vectorization
+% Whitenning, logarithm transform, and vectorization
 Cov_whiten = zeros(M, KC, KC);
 for m = 1:M
-    temp_cov = Cov_mean_train^(-1/2)*Cov{1,m}*Cov_mean_train^(-1/2);
-    Cov_whiten(m,:,:) = (temp_cov + temp_cov')/2; 
+    temp_cov = Wh^(-1/2)*Cov{1,m}*Wh^(-1/2);
+    Cov_whiten(m,:,:) = (temp_cov + temp_cov')/2;
     R_m =logm(squeeze(Cov_whiten(m,:,:))); % logarithm transform
     R_m = R_m(:); % column-wise vectorization
-    R_test(m,:) = R_m';   
-end 
+    R_test(m,:) = R_m';
+end
 end
 
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-function [accuracy] = compute_acc (predict_Y, Y_test)
-% Calculate class label 
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function accuracy = compute_acc (predict_Y, Y_test)
+% Compute classification accuracy for test set
 Y_predict = zeros(length(predict_Y),1);
 for i = 1:length(predict_Y)
     if (predict_Y(i) > 0)
@@ -95,9 +83,9 @@ for i = 1:length(predict_Y)
     else
         Y_predict(i) = -1;
     end
-end      
-% Compute accuracy
-error_num = 0; 
+end
+% Compute classification accuracy
+error_num = 0;
 total_num = length(predict_Y);
 for i = 1:total_num
     if (Y_predict(i) ~= Y_test(i))
