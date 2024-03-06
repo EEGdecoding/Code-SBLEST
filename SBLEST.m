7465f55116b0e4a9577b7e354169fb2027704428
EEGdecoding <109576028+EEGdecoding@users.noreply.github.com>
2024-03-06 11:07:44 +0800

    SBLEST.m

--- SBLEST.m
+++ SBLEST.m
@@ -181,7 +181,9 @@ function log_det_X = calculate_log_det(X)
     n = size(X,1); % Get the size of matrix X
     c = 10^floor(log10(X(1,1))); % Extract the scaling factor c as a power of 10
     A = X / c; % Normalize the matrix by the scaling factor
-    log_det_A = log(det(A)); % Compute the log determinant of the normalized matrix
+    L = chol(A, 'lower'); % Perform Cholesky decomposition on A
+    log_det_A= 2 * sum(log(diag(L)));% Compute the log determinant of the normalized matrix via L
+%      log_det_A = log(det(A)); 
     log_det_X = n*log(c) + log_det_A; % Combine the results to get the log determinant of the original matrix
 end
 
